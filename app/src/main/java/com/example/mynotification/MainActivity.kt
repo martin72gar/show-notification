8a451a0f131aed6d91cfd1a95296dd34ac854845
Office PC <72garmartin@gmail.com>
2021-10-15 09:52:10 +0700

    pending intent to open app from notification

--- app/src/main/java/com/example/mynotification/MainActivity.kt
+++ app/src/main/java/com/example/mynotification/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.mynotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -10,23 +13,33 @@ import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     val CHANNEL_ID = "channelID"
     val CHANNEL_NAME = "channelName"
-    val NOTIFICATION_ID = 0
+    val NOTIFICATION_ID = 0 //the notification replace with new notification
+//    val NOTIFICATION_ID = Random.nextInt()  //the new notification added
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createNotificationChannel()
 
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Awesome Notification")
             .setContentText("This is the content text")
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
